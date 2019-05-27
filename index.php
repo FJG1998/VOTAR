f07bd88dbd921b93aa28654ddae0ee5d47b550af
fjimenezjob@gmail.com <franjimenez@hotelbeds.com>
2019-05-27 11:07:05 +0200

    base lista

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DNI</title>
+</head>
+<body>
+
+    <form action="" method="POST">
+
+                
+        <input type="text" name="dni" id="" placeholder="DNI" required>
+
+        <input type="submit" value="COMPROVAR">
+
+    </form>
+    
+</body>
+</html>
